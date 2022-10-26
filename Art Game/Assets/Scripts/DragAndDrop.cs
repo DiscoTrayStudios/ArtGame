a1431c5f9d232c076373bcbd5e77833500c94854
Ian Campbell <campbellni@hendrix.edu>
2022-10-26 15:54:17 -0500

    Drag and Drop puzzle pieces

---
+++ Art Game/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject selectedPiece;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (selectedPiece == null)
+            {
+                RaycastHit  hit= castray();
+                if (hit.collider != null)
+                {
+                    if (!hit.collider.CompareTag("puzzle"))
+                    {
+                        return;
+                    }
+                    selectedPiece = hit.collider.gameObject;
+                    Cursor.visible = false;
+                }
+            }
+            else
+            {
+                Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(selectedPiece.transform.position).z);
+                Vector3 worldpos = Camera.main.ScreenToWorldPoint(position);
+                selectedPiece.transform.position = new Vector3(worldpos.x, worldpos.y, worldpos.z);
+
+                selectedPiece = null;
+                Cursor.visible = true;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectedPiece = null;
+            Cursor.visible = true;
+        }
+
+
+        if (selectedPiece != null)
+        {
+            Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(selectedPiece.transform.position).z);
+            Vector3 worldpos = Camera.main.ScreenToWorldPoint(position);
+            selectedPiece.transform.position = new Vector3(worldpos.x, worldpos.y, worldpos.z);
+
+
+        }
+    }
+
+
+    private RaycastHit castray()
+    {
+        Vector3 screenmouseposfar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
+        Vector3 screenmouseposnear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+        Vector3 worldmouseposfar = Camera.main.ScreenToWorldPoint(screenmouseposfar);
+        Vector3 worldmouseposnear = Camera.main.ScreenToWorldPoint(screenmouseposnear);
+        RaycastHit hit;
+        Physics.Raycast(worldmouseposnear, worldmouseposfar - worldmouseposnear, out hit);
+        return hit;
+    }
+}
