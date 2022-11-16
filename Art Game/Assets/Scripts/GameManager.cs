38276e96cad8c1aa8758dba8f7a90d2089cc1e57
Ian Campbell <campbellni@hendrix.edu>
2022-11-16 13:44:10 -0600

    Completion code, backgrounds, starting on transitions

--- Art Game/Assets/Scripts/GameManager.cs
+++ Art Game/Assets/Scripts/GameManager.cs
@@ -11,8 +11,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     // Start is called before the first frame update
-    public Canvas inventory;
-    public Canvas pauseMenu;
+    public Canvas returnButton;
+    public Canvas infoScreen;
+    public TextMeshProUGUI infotext;
     public GameObject player;
     public Camera cam;
     public GameObject pbn;
@@ -30,6 +31,8 @@ public class GameManager : MonoBehaviour
     private int ispyCounter;
     private int wordCounter;
     private int pbnCounter;
+    private bool lerping = false;
+    private bool lerpforward;
 
     private void Awake()
     {
@@ -46,6 +49,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         camStartPos = cam.transform.position;
         camStartRot = cam.transform.rotation;
         ispyCounter = 0;
@@ -56,45 +60,76 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (lerping)
+        {
+            if (lerpforward)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    public void setLerp(bool val)
+    {
+        lerping = val;
+    }
+    public void setLerpForward(bool val)
+    {
+        lerpforward = val;
     }
 
 
     public void buttonPress(string name)
     {
-        cam.orthographic = true;
-        if (name.Equals("luncheon"))
-        {
-            cam.transform.position = new Vector3(-4f, 12f, -22f);
-            cam.transform.eulerAngles = new Vector3(0, 180f, 0);
-            game = name;
-        }
-        else if (name.Equals("lilypads"))
-        {
-            cam.transform.position = new Vector3(2.16f, 10.84f, -20.54f);
-            cam.GetComponent<DragAndDrop>().enabled = true;
-            game = name;
-        }
-        else if (name.Equals("and i was there"))
-        {
-            cam.transform.position = new Vector3(26.75f, 45.48f, -20.54f);
-            game = name;
-        }
-        else if (name.Equals("ice pies"))
-        {
-            cam.transform.position = new Vector3(21.3f, 73.1f, -22f);
-            cam.transform.eulerAngles = new Vector3(0, 180f, 0);
-            game = name;
 
-        }
-        else if (name.Equals("crossing"))
+        game = name;
+        
+        if (!lerping)
         {
-            cam.transform.position = new Vector3(59.5f, 10.2f, -9.6f);
-            cam.transform.eulerAngles = new Vector3(0.0488541909f, 359.9f, 0.0226515327f);
-            cam.orthographic = false;
-            slider.GetComponent<ST_PuzzleDisplay>().actualStart();
+            if (name.Equals("luncheon"))
+            {
 
+                cam.transform.position = new Vector3(-4f, 12f, -22f);
+                cam.transform.eulerAngles = new Vector3(0, 180f, 0);
+            }
+            else if (name.Equals("lilypads"))
+            {
+                cam.transform.position = new Vector3(-2.94f, 10.84f, -20.55f);
+                cam.GetComponent<DragAndDrop>().enabled = true;
+            }
+            else if (name.Equals("and i was there"))
+            {
+                cam.transform.position = new Vector3(26.75f, 45.48f, -20.54f);
+            }
+            else if (name.Equals("ice pies"))
+            {
+                cam.transform.position = new Vector3(21.3f, 73.1f, -22f);
+                cam.transform.eulerAngles = new Vector3(0, 180f, 0);
+
+            }
+            else if (name.Equals("crossing"))
+            {
+                cam.transform.position = new Vector3(59.5f, 10.2f, -9.6f);
+                cam.transform.eulerAngles = new Vector3(0.0488541909f, 359.9f, 0.0226515327f);
+
+
+
+            }
+
+            displayInfoText();
+            cam.orthographic = true;
+            if (name.Equals("crossing"))
+            {
+                cam.orthographic = false;
+                slider.GetComponent<ST_PuzzleDisplay>().actualStart();
+
+            }
         }
+
     }
 
     public string getGame()
@@ -180,11 +215,46 @@ public class GameManager : MonoBehaviour
         return true;
     }
 
+    public void displayInfoText()
+    {
+        returnButton.gameObject.active = false;
+        infoScreen.gameObject.active = true;
+        if (game.Equals("luncheon"))
+        {
 
+            infotext.text = "Tap on the hidden objects to complete the game!";
+        }
+        else if (game.Equals("lilypads"))
+        {
+            infotext.text = "Drag and Drop the jigsaw pieces into the correct position! Correctly placed pieces will stay in place.";
+        }
+        else if (game.Equals("and i was there"))
+        {
+            infotext.text = "Paint each section with the matching numbered paint. Tap on a paint to select it, then the painting to use it!";
+
+        }
+        else if (game.Equals("ice pies"))
+        {
+            infotext.text = "Tap the hidden words and phrases in the picture!";
+
+        }
+        else if (game.Equals("crossing"))
+        {
+            infotext.text = "Tap on the pieces to slide them over and try and get the original picture! The bottom left square will be missing when the puzzle is complete.";
+        }
+        Time.timeScale = 0f;
+    }
+
+    public void hideInfoText()
+    {
+        returnButton.gameObject.active = true;
+        infoScreen.gameObject.active = false;
+        Time.timeScale = 1f;
+    }
     public void quitGame()
     {
+        returnButton.gameObject.active = false;
         cam.GetComponent<DragAndDrop>().enabled = false;
-        Debug.Log("YOUUUUUUUU");
         cam.transform.position = camStartPos;
         cam.transform.rotation = camStartRot;
         cam.orthographic = false;
--- Art Game/Assets/Scripts/Snap.cs
+++ Art Game/Assets/Scripts/Snap.cs
@@ -9,7 +9,7 @@ public class Snap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        originalPos = gameObject.transform.position;
+        originalPos = gameObject.transform.localPosition;
         randomizeLocation();
         canSnap = true;
     }
@@ -17,9 +17,9 @@ public class Snap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canSnap && Vector3.Distance(gameObject.transform.position, originalPos) < 0.5f)
+        if (canSnap && Vector3.Distance(gameObject.transform.localPosition, originalPos) < 0.5f)
         {
-            gameObject.transform.position = originalPos;
+            gameObject.transform.localPosition = originalPos;
             canSnap = false;
             if (GameManager.Instance.checkPuzzle())
             {
@@ -31,8 +31,21 @@ public class Snap : MonoBehaviour
 
     public void randomizeLocation()
     {
-        Vector3 tempPos = new Vector3(Random.Range(-15.5f, 11f), Random.Range(2f, 18f), 1.55f);
-        gameObject.transform.position = tempPos;
+        
+        while (true)
+        {
+            float newx = Random.Range(-8.5f, 13f);
+            float newy = Random.Range(-8f, 2.5f);
+            if (newx < -4.5f || newx > 9.5f || newy < -7.5f || newy > 1f)
+            {
+                
+                    Vector3 tempPos = new Vector3(newx, newy, 0f);
+                    gameObject.transform.localPosition = tempPos;
+                    break;
+                
+            }
+        }
+        
     }
 
     public bool getcanSnap()
